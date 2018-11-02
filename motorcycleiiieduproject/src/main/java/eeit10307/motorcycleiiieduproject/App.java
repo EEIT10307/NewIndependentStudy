41f3319d946cd3c2482cfe12983ae977a8d487d4
陳冠穎 <eternal221950@gmail.com>
2018-11-02 16:51:25 +0800

    test commit

---
+++ motorcycleiiieduproject/src/main/java/eeit10307/motorcycleiiieduproject/App.java
@@ -0,0 +1,13 @@
+package eeit10307.motorcycleiiieduproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
