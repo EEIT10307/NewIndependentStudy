377364fdb41489be96698f8e5aae46a62101321e
楊壬賓 <bookwater@yahoo.com.tw>
2018-11-08 20:51:51 +0800

    照片上傳修正  正式版1.0

--- motorcycleiiieduproject/src/main/java/everybikeInfocontroller/TestRobinController.java
+++ motorcycleiiieduproject/src/main/java/everybikeInfocontroller/TestRobinController.java
@@ -14,8 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
