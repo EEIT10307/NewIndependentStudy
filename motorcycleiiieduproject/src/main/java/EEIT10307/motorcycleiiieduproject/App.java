bd0dbae1fedd4b297d37bd4be9a766febc996600
陳冠穎 <kuanyingchen@172.20.10.4>
2018-11-14 15:17:05 +0800

    mac first commit

---
+++ motorcycleiiieduproject/src/main/java/EEIT10307/motorcycleiiieduproject/App.java
@@ -0,0 +1,13 @@
+package EEIT10307.motorcycleiiieduproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
