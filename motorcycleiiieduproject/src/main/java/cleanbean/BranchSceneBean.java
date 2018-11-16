524860fb99d265aa5f228461597a5c29ee1b9720
駱瑭芸 <c512andy@yahoo.com.tw>
2018-11-16 17:40:32 +0800

    抓地區景點成功

---
+++ motorcycleiiieduproject/src/main/java/cleanbean/BranchSceneBean.java
@@ -0,0 +1,18 @@
+package cleanbean;
+
+public class BranchSceneBean {
+	private String spotArea;
+
+	public String getSpotArea() {
+		return spotArea;
+	}
+
+	public void setSpotArea(String spotArea) {
+		this.spotArea = spotArea;
+	}
+
+	@Override
+	public String toString() {
+		return "BranchSceneBean [spotArea=" + spotArea + "]";
+	}
+}
